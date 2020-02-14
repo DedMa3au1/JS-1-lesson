0f23e9a9ca48e55afbee18f81970850d97c2d396
Ded_Ma3au <samandaragzamov1@gmail.com>
2020-02-14 09:37:06 +0500

    Add Project JS

---
+++ script.js
@@ -0,0 +1,12 @@
+var userName = prompt('Ваше Имя')
+console.log('Я ' +userName);
+var userDate = prompt('Ваш Возраст')
+console.log('Мне ' +userDate);
+var num1 = prompt('Решите Задачу: 7*3= ')
+console.log('Пример 1: 7*3=21 : Ваш ответ/' +num1)
+var num2 = prompt('Решите Задачу: 7+3= ')
+console.log('Пример 1: 7+3=10 : Ваш ответ/' +num2)
+var num3 = prompt('Решите Задачу: 7-3= ')
+console.log('Пример 1: 7-3=4 : Ваш ответ/' +num3)
+var num4 = prompt('Решите Задачу: 7%3= ')
+console.log('Пример 1: 7%3=1 : Ваш ответ/' +num4)
